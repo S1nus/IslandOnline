72c7b2a095cf94af7bf197fd094ecae5d3909927
S1nus <chuckhead7@gmail.com>
2017-06-25 09:01:04 -0400

    Attempt to post data to server
    Server doesn't work :/

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
+var app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -11,6 +17,10 @@ app.get(/^(.+)$/, function(req, res) {
   console.log("static file request: " + req.params);
   res.sendFile(__dirname + req.params[0]);
 });
+/*
+app.post('/meme', function(req, res) {
+  console.log(req.body.username);
+});*/
 
 io.on('connection', function(socket) {
   console.log("A client has connected");
